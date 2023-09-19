80c3368aa12cc0acb1c44007a85fb0962a119741
Loan Vitor <100142681+LoanVitor@users.noreply.github.com>
2023-09-19 00:30:18 +0000

    Pending changes exported from your codespace

---
+++ jwt_auth/config/custom_environment-viriables.ts
@@ -0,0 +1,6 @@
+export default {
+    dbName: 'MONGODB_USERNAME',
+    dbPass: 'MONGODB_PASSWORD',
+    acessTokenPrivateKey: 'ACESS_TOKEN_PRIVATE_KEY',
+    acessTokePublicKey: 'ACCESS_TOKEN_PUBLIC_KEY',
+};
---
+++ jwt_auth/config/default.ts
@@ -0,0 +1,5 @@
+export default {
+    port: 8000,
+    acessTokenExpiresIn: 15,
+    origien: 'http://localhost:300',
+}
